7a285b2fdae6aae5b9bb77759850c3f2038787a6
Violetta <forbes.life@mail.ru>
2022-04-10 19:20:09 +0300

    Нахождение четного числа

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 7;
+
+if(number % 2 == 0)
+{
+    Console.Write("четное");
+    Console.WriteLine(number);
+}
+else 
+{
+    Console.Write("нечетное");
+    Console.WriteLine(number);
+}
