4102641f34360b0eda61dea14c57a03f3f2a8dd2
Violetta <forbes.life@mail.ru>
2022-04-10 19:51:28 +0300

    Нахождение наименьшего и наибольшего числа

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,13 +1,19 @@
-﻿int a = -9;
-int b = -3;
+﻿int a = 10;
+int b = 5;
 
 int max = a;
+int min = b;
 
 if(a > max) max = a;
 if(b > max) max = b;
-
+{
     Console.Write("max= ");
     Console.WriteLine(max);
-
-
+}
+if(a < min) min = a;
+if(b < min) min = b;
+{
+    Console.Write("min= ");
+    Console.WriteLine(min);
+}
 
